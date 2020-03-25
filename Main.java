afd93d78c55aaa4ecd8f962a0867f4f1d1dde092
Git Server Admin <admin@git-server>
2020-03-25 10:21:32 +0000

    Add initial repository

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+  static String originalString="abcabc";
+  
+  private static int operationCount(String word, int input1,int input2){
+        
+      /*write your code here and return appropriate value*/
+    return 0;
+     
+  }
+  
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)
+  {
+     originalString=args[0];
+    System.out.println(operationCount(args[0],Integer.parseInt(args[1]),Integer.parseInt(args[2])));
+  }
+}
