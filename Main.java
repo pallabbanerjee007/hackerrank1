e5a16c2298da329578fd3a5c5f52a70183792410
pallabbanerjee007 <62647149+pallabbanerjee007@users.noreply.github.com>
2020-03-25 16:31:36 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,22 @@ public class Main
   private static int operationCount(String word, int input1,int input2){
         
       /*write your code here and return appropriate value*/
+    int count = 0;
+    String originalWord = word; 
+    int length = word.length();
+    while(input1 < length && input2 < length) {
+    for(int i=0;i<500;i++) {
+      
+    word = word.substring(length-input1, length).concat(word.substring(0, length-input1));
+    word = word.substring(length-input2, length).concat(word.substring(0, length-input2));
+      count ++;
+    if(originalWord.compareTo(word) == 0) {
+    return count;
+    }
+    }
+    
     return 0;
-     
+    
   }
   
   // arguments are passed using the text field below this editor
